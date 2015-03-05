b1715cdeccfadf34d7ef62062ad280b0e239bd50
dev_brian_cho <kwang8992@gmail.com>
2015-03-05 23:11:00 +0900

    click eventListener에서 wordList를 사용할 수 있도록 printOnDiv()의 구조를 변경 & removeData 모듈 완성

--- main.js
+++ main.js
@@ -9,6 +9,21 @@ function xhrGet(url, callback) {
     xhr.send()
 };
 
+function onWindowLoad() {
+
+  var message = document.querySelector('#message');
+
+  chrome.tabs.executeScript(null, {
+    file: "getPagesSource.js"
+  }, function() {
+    // If you try and inject into an extensions page or the webstore/NTP you'll get an error
+    if (chrome.extension.lastError) {
+      message.innerText = 'There was an error injecting script : \n' + chrome.extension.lastError.message;
+    }
+  });
+
+};
+
 chrome.extension.onMessage.addListener(function(request, sender) {
   if (request.action == "getSource") {
     var sampleText = strip_tags(request.source);
@@ -85,54 +100,52 @@ function translateWords(wordDictionary) {
 //클라이언트에서 처리할 수 있는 모든 처리를 끝마친 최종데이터, 출력과 서버전송에 사용한다.
 function dataProvider(wordList, dictListLength){
   //현재 단어를 UI로 구성하는 로직
-  for (var i in wordList){ 
-    // console.log(i+"::: dictList."+wordList[i].key+" = "+wordList[i].mean)
-    printOnDiv(wordList[i]);
-  }
-  
-  //삭제할 단어를 담을 변수
-  var deleteKey = [];
+  printOnDiv(wordList);
 
   //데이터를 검색하기 위한 로직
   searchWord(wordList);
 };
 
-function printOnDiv(wordObject) {
+function printOnDiv(wordList) {
   //웹 출력 함수 // 처음에는 베이직한 div 보내고 나중에 처리 다 끝나고 우선순위 추출알고리즘 다 돌리면 특정 색상으로 변환하는
   //혹은 현재 화면에 보일만한 예를들면 한 7~10줄정도의 데이터만 후딱 우선순위 알고리즘 돌려서 처리하고 나머지는 비동기로 천천히 처리해도 괜찮을듯.
 
-  //div 생성
-  var div = document.createElement('div');
-  div.setAttribute('class', 'tile');
-  div.setAttribute('id', wordObject.key);
-  document.body.appendChild(div);
+  for (var i in wordList){ 
+    //div 생성
+    var div = document.createElement('div');
+    div.setAttribute('class', 'tile');
+    div.setAttribute('id', wordList[i].key);
+    document.body.appendChild(div);
 
-  div.innerHTML=wordObject.key+"<br>"+wordObject.mean[0]+", "+wordObject.mean[1];
+    // console.log("wordList["+i+"].key = "+wordList[i].key+"   :   wordList["+i+"].mean = "+wordList[i].mean);
+    div.innerHTML=wordList[i].key+"<br>"+wordList[i].mean[0]+", "+wordList[i].mean[1];
 
-  // 버튼 리스너
-  var searchBtn = document.getElementById(wordObject.key);
-  searchBtn.addEventListener('click', function(event) {
-    //현재페이지의 단어를 담은 Div 삭제 로직
-    var removeDiv = document.getElementById(wordObject.key);
-    removeDiv.remove(removeDiv);
-    console.log("remove "+wordObject.key);
-  });
+    // 버튼 리스너
+    var clickBtn = document.getElementById(wordList[i].key);
+    clickBtn.addEventListener('click', function(event) {
+      var selectId = event.target.id;
+      
+      //현재페이지의 단어를 담은 Div 삭제 로직
+      var removeDiv = document.getElementById(selectId);
+      removeDiv.remove(selectId);
+
+      //현재페이지의 단어를 모아놓는 Dictionary에서 해당 단어 제거 로직
+      removeData(wordList, selectId);
+
+      //서버의 DB에 해당 데이터 삭제를 요청하는 로직
+      console.log("We need Server");
+    });
+  }
 };
 
-function removeData(selectKey) {
+function removeData(wordList, selectId) {
   //현재 페이지의 단어를 모아놓는 Dictionary에서 해당 단어 제거 로직
-  if (wordList.length == dictListLength) {
-    for (var i=0; i<wordList.length; i++) {
-      // console.log("wordList["+i+"] = "+wordList[i].key);
-      
-      //dictionary에서 원소 제거하는방법
-      // delete thisIsObject[key];
-      
+  for (var i=0; i<wordList.length; i++) {
+    // console.log("wordList["+i+"] = "+wordList[i].key);
+    if (wordList[i].key == selectId) {
+      delete wordList[i];
     }
   }
-
-  //DB에 저장되어있는 단어를 제거하는 로직
-  console.log("wordList to Server");
 };
 
 function searchWord() {
