798813cc96ad9cb1a13101a0fb9298b4bfed55ed
dev_brian_cho <kwang8992@gmail.com>
2015-03-11 16:52:41 +0900

    검색할 단어를 입력받는 search() 모듈과 단어 keyword에 해당하는 div를 보여주는(나머지 div는 숨겨버리는) displaySearchResult() 모듈 완성.

--- main.js
+++ main.js
@@ -77,7 +77,7 @@ function translateWords(wordDictionary) {
   var keys = Object.keys(wordDictionary);
     // var index = parseInt(Math.random()*keys.length);
     var keyCount=0;
-    var wordList = {};
+    var wordDict = {};
     for(var i =0; i<keys.length; i++){
       var url = "http://tooltip.dic.naver.com/tooltip.nhn?wordString=" + keys[i] + "&languageCode=4&nlp=false";
       xhrGet(url, function(xhr, callback) {
@@ -86,42 +86,79 @@ function translateWords(wordDictionary) {
         if(obj.hasOwnProperty('entryName')){
           var word = obj['entryName'];
           var mean = obj['mean'];
-          if(!wordList.hasOwnProperty(word))          
-            wordList[word] = {'key':word,'mean':mean};
+          if(!wordDict.hasOwnProperty(word))          
+            wordDict[word] = {'key':word,'mean':mean};
         }
 
         if(keyCount==keys.length){
-          dataProvider(wordList);
+          dataProvider(wordDict);
         }
       });
     }
 };
 
 //클라이언트에서 처리할 수 있는 모든 처리를 끝마친 최종데이터, 출력과 서버전송에 사용한다.
-function dataProvider(wordList){
+var awordDict={};
+function dataProvider(wordDict){
   //현재 단어를 UI로 구성하는 로직
-  printOnDiv(wordList);
-
+  printOnDiv(wordDict);
+  awordDict = wordDict
   //데이터를 검색하기 위한 로직
-  searchWord(wordList);
+  search();
 };
 
-function printOnDiv(wordList) {
+//검색어를 입력받는 keydown eventListener
+function search() {
+  document.getElementById("inputString").addEventListener('keydown',function(event){
+    var inputString = String.fromCharCode(event.keyCode);
+    var originString = document.getElementById("inputString").value; 
+
+    // console.log("***"+inputString.toLowerCase()+document.getElementById("inputString").value);
+
+    if(event.keyCode == 13) {
+      //엔터부분
+    } else if (65<=event.keyCode && event.keyCode <= 90) {
+      originString += inputString.toLowerCase();
+      displaySearchResult(originString);
+      console.log("complete input = " +originString);
+    } else if (event.keyCode == 8) {
+      //text = text.substring(0, text.length-1);
+      originString = originString.substring(0, originString.length-1);
+      console.log("press backspace = " +originString);
+      displaySearchResult(originString);
+    }
+  });
+};
+
+//textbox에 입력된 데이터와 wordDict의 데이터를 비교후 div 제어
+function displaySearchResult(findWord) {
+  console.log("findWord out of loop = "+findWord);
+  for(var key in awordDict){
+    if(key.indexOf(findWord)==0){
+      console.log("findWord = "+findWord);
+      document.getElementById(key).style.display="block";
+    }else{
+      document.getElementById(key).style.display="none";
+    }
+  }
+};
+
+function printOnDiv(wordDict) {
   //웹 출력 함수 // 처음에는 베이직한 div 보내고 나중에 처리 다 끝나고 우선순위 추출알고리즘 다 돌리면 특정 색상으로 변환하는
   //혹은 현재 화면에 보일만한 예를들면 한 7~10줄정도의 데이터만 후딱 우선순위 알고리즘 돌려서 처리하고 나머지는 비동기로 천천히 처리해도 괜찮을듯.
 
-  for (var i in wordList){ 
+  for (var i in wordDict){
     //div 생성
     var div = document.createElement('div');
     div.setAttribute('class', 'tile');
-    div.setAttribute('id', wordList[i].key);
+    div.setAttribute('id', i);
     document.body.appendChild(div);
 
-    // console.log("wordList["+i+"].key = "+wordList[i].key+"   :   wordList["+i+"].mean = "+wordList[i].mean);
-    div.innerHTML=wordList[i].key+"<br>"+wordList[i].mean[0]+", "+wordList[i].mean[1];
+    // console.log("wordDict["+i+"].key = "+wordDict[i].key+"   :   wordDict["+i+"].mean = "+wordDict[i].mean);
+    div.innerHTML=wordDict[i].key+"<br>"+wordDict[i].mean[0]+", "+wordDict[i].mean[1];
 
     // 버튼 리스너
-    var clickBtn = document.getElementById(wordList[i].key);
+    var clickBtn = document.getElementById(wordDict[i].key);
     clickBtn.addEventListener('click', function(event) {
       var selectId = event.target.id;
       
@@ -130,7 +167,7 @@ function printOnDiv(wordList) {
       removeDiv.remove(selectId);
 
       //현재페이지의 단어를 모아놓는 Dictionary에서 해당 단어 제거 로직
-      removeData(wordList, selectId);
+      removeData(wordDict, selectId);
 
       //서버의 DB에 해당 데이터 삭제를 요청하는 로직
       console.log("We need Server");
@@ -138,17 +175,12 @@ function printOnDiv(wordList) {
   }
 };
 
-function removeData(wordList, selectId) {
+function removeData(wordDict, selectId) {
   //현재 페이지의 단어를 모아놓는 Dictionary에서 해당 단어 제거 로직
   console.log("selectId = "+selectId);
-  delete wordList[selectId];
-  console.log(selectId+':'+wordList.hasOwnProperty(selectId));
+  delete wordDict[selectId];
+  console.log(selectId+':'+wordDict.hasOwnProperty(selectId));
   
-};
-
-function searchWord() {
-  // 리스너가 호출되면
-  // 
 };
 
 window.onload = onWindowLoad;
